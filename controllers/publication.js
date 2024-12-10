a5dc3446be0834fe97267c9ef88e2a4823d4ce2f
Armando1509 <armandogomez1509@gmail.com>
2024-12-10 13:13:35 -0600

    creacion de funcion para traer publicaciones de un solo usuario

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,4 +1,3 @@
-
 const Publication = require("../models/publication");
 
 //Accion de prueba
@@ -84,18 +83,21 @@ const one = async (req, res) => {
 // Eliminar publicaciones
 
 const remove = async (req, res) => {
-    // traer al usuario
-    let userId = req.user.id
+  // traer al usuario
+  let userId = req.user.id;
   // Traer el Id por params
   let publicationId = req.params.id;
 
   try {
-    let removePublication = await Publication.deleteOne({ _id: publicationId})
-    if(!removePublication){
-        return res.status(404).send({
-            status: "Error",
-            message: "No se a podido eliminar la publicacion",
-          });
+    let removePublication = await Publication.deleteOne({
+      user: req.user.id, // esto es para que solo el usuario identificado borre sus propias publicaciones
+      _id: publicationId,
+    });
+    if (!removePublication) {
+      return res.status(404).send({
+        status: "Error",
+        message: "No se a podido eliminar la publicacion",
+      });
     }
     return res.status(200).send({
       status: "Success",
@@ -112,9 +114,46 @@ const remove = async (req, res) => {
   }
 };
 
+// Listar las publicaciones de un usuario
+
+const user = async (req, res) => {
+  // Traer el id
+  const userId = req.params.id;
+  // La paginacio
+  let page = 1;
+  if (req.params.page) page = req.params.page;
+  let itemsPerPage = 5;
+  try {
+    let total = await Publication.countDocuments();
+    let publications = await Publication.find({ user: userId })
+    .sort("-create_at")
+    .populate("user", "-create_at -__v -password")
+    .paginate(page, itemsPerPage);
+    if(publications.length <= 0){
+      return res.status(404).send({
+        status: "Error",
+        message: "No hay publicaciones para mostrar",
+      });
+    }
+    return res.status(200).send({
+      status: "Success",
+      message: "Aqui tienes la lista de publicaciones",
+      total,
+      pages: Math.ceil(total / itemsPerPage),
+      publications,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      status: "Error",
+      message: "Ponte a llorar",
+    });
+  }
+};
+
 module.exports = {
   pruebaPublication,
   save,
   one,
   remove,
+  user,
 };
--- routes/publication.js
+++ routes/publication.js
@@ -7,5 +7,6 @@ router.get("/prueba-publication", PublicationController.pruebaPublication)
 router.post("/save", check.auth, PublicationController.save)
 router.get("/one/:id",check.auth, PublicationController.one)
 router.delete("/remove/:id", check.auth, PublicationController.remove)
+router.get("/user/:id/:page?", check.auth, PublicationController.user)
 // Exportar ruta
 module.exports = router
